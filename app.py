c75022e4eed0513fd12be2b5680714f0b18653d5
khoalalung <125677623+khoalalung@users.noreply.github.com>
2023-03-16 22:08:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LinearRegression
 
 model = pickle.load(open('model.pickle', "rb"))
-y_new = model.predict(x_new)
 
 st.title('Revenue Prediction')
 x_new = st.number_input('Input Temperature')
+y_new = model.predict(x_new)
+
 if st. button('Predict'):
 	st.write('Revenue Prediction', y_new)
