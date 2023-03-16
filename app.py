beecf6c41efc50c0770a7e3cdaa2dcef328744f3
khoalalung <125677623+khoalalung@users.noreply.github.com>
2023-03-16 21:45:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pickle
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
 
 model = pickle.load(open(filename, "rb"))
+y_new = model.predict(x_new)
+
 st.title('Revenue Prediction')
 x_new = st.number_input('Input Temperature')
 if st. button('Predict'):
